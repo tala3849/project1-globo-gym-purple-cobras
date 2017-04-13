e7844e9a638c4d5755e1ba81539205d95eb9adfd
Jennings Anderson <jennings.anderson@colorado.edu>
2017-04-13 15:01:24 -0600

    adding loaders and stuff

--- docs/js/loader.js
+++ docs/js/loader.js
@@ -1,10 +1,16 @@
 /*  Load scripts here  */
 
-var loadData = function(config){
+
+/* This is the main page run time, because as it loads the documents,
+   it must call individual visualizations
+*/
+
+var load = function(config){
   this.dir = config.dir
+
+  this.d3.csv(dir+'/Buildings.csv',function(data){
+
+  })
 }
-
-
-
 
 loadData({dir: 'data/'})
--- docs/js/page.js
+++ docs/js/page.js
@@ -1,10 +1,20 @@
 console.log("Page Loaded")
 
 /*
-  Page functions should go here
+  General Page functions should go here
 */
 
 function toggleVis(btn, id){
   document.getElementById(id).style.display = (document.getElementById(id).style.display=='block')? 'none' : 'block'
   document.getElementById(btn).className = (document.getElementById(btn).className=='btn')? 'btn btn--stroke' : 'btn';
+}
+
+var DataObject = function(config){
+  this.dir = config.dir
+
+  this.load = function(){
+    d3.csv(dir+'/ZayoHackathonData_Buildings.csv',function(data){
+      console.log('loaded buildings')
+    })
+  })
 }
