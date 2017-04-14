20d664fbfbc284663fa1ebd83bb73c42fdfa47bb
Melissa <melissa.j.bica@gmail.com>
2017-04-14 13:55:31 -0600

    Updated bar chart

--- docs/js/products.js
+++ docs/js/products.js
@@ -5,11 +5,11 @@ function createProductsVis(){
 	console.log(pageData.products)
 
 
-    var w = 600; // Width of our visualization
-    var h = 400; // Height of our visualization
-    var xOffset = 60; // Space for x-axis labels
-    var yOffset = 40; // Space for y-axis labels
-    var margin = 10; // Margin around visualization
+    var width = 600; // Width of our visualization
+    var height = 600; // Height of our visualization
+    var xOffset = 60; // Space for y-axis labels
+    var yOffset = 150; // Space for x-axis labels
+    var margin = 20; // Margin around visualization
     var vals = ['x', 'y']; // List of data attributes
     var xVal = vals[0]; // Value to plot on x-axis
     var yVal = vals[1]; // Value to plot on y-axis
@@ -18,14 +18,13 @@ function createProductsVis(){
     var zayoOrange = '#f58233'
     var zayoTeal = '#005d77'
 
-
-    var data = pageData.products;
+    var length = 12;
+    var data = pageData.products.slice(0,length);
     data.forEach(function(d,idx){
-    	// console.log(d["Product Group"], d["Count"])
         d.product = d["Product Group"];
         d.idx = idx;
-        d.y = +d["Count"];
-        console.log(idx, d.product, d.y)
+        d.count = +d["Count"];
+        console.log(idx, d.product, d.count)
     });
 
     // Define scales that convert from the data domain to screen coordinates
@@ -34,39 +33,55 @@ function createProductsVis(){
     // into screen coordinates (.range([min pixel value, max pixel value])).
     // Using linear scales maps the data directly to the pixel values using
     // pixel_val = c * data_val, where c is a constant computed by d3.
-    var xScale = d3.scale.linear()
-                    .domain([0, data.length])
-                    .range([xOffset + margin, w - margin]);
+
+    // var xScale = d3.scale.linear()
+    //                 .domain([0, data.length])
+    //                 .range([xOffset + margin, w - margin]);
+    var xScale = d3.scale.ordinal()
+            .rangeRoundBands([xOffset + margin, width - margin],.1)
+            .domain(data.map(function(d) { return d.product; }));
     
     var yScale = d3.scale.linear()
-                    .domain([0, d3.max(data, function(d) { return parseFloat(d.y); })+1])
-                    .range([h - yOffset - margin, margin]);
+                    .domain([0, d3.max(data, function(d) { return parseFloat(d.count); })+1])
+                    .range([height - yOffset - margin, margin]);
     
     // Next, we will create an SVG element to contain our visualization.
     var svg = d3.select("#barchart").append("svg:svg")
-                                    .attr("width", w)
-                                    .attr("height", h);
+                                    .attr("width", width+30)
+                                    .attr("height", height);
     
 
     // Build axes! (These are kind of annoying, actually...)
     // Specify the axis scale and general position
     var xAxis = d3.svg.axis()
+    				  // .attr("id","xaxis")
                       .scale(xScale)
                       .orient("bottom")
-                      .ticks(5);
+                      // .ticks(5);
     
     // Add a graphics element to hold the axis we created above (xAxis)
+    // var xAxisG = svg.append('g')
+    //                 .attr('class', 'axis')
+    //                 .attr('transform', 'translate(0, ' + (h - yOffset) + ')')
+    //                 .call(xAxis);
+
     var xAxisG = svg.append('g')
                     .attr('class', 'axis')
-                    .attr('transform', 'translate(0, ' + (h - yOffset) + ')')
-                    .call(xAxis);
-    
+                    .attr("transform", "translate(0," + (height-yOffset) + ")")
+                    .call(xAxis)
+				  .selectAll("text")
+				    .attr("y", 10)
+				    .attr("x", 9)
+				    .attr("dy", ".35em")
+				    .attr("transform", "rotate(45)")
+				    .style("text-anchor", "start");
+				    
     // Add a label that shows the user what that axis represents
     var xLabel = svg.append("text")
                     .attr('class', 'label')
-                    .attr('x', (w)/2 + 2*margin)
-                    .attr('y', h - margin/2)
-                    .text("Product");
+                    .attr('x', (width)/2 + 2*margin)
+                    .attr('y', height - margin/2)
+                    .text("Product")
     
     // Repeat for the y-axis
     var yAxis = d3.svg.axis()
@@ -82,14 +97,14 @@ function createProductsVis(){
     var yLabel = svg.append("text")
                     .attr('class', 'label')
                     .attr('x', xOffset/2)
-                    .attr('y', h/2)
+                    .attr('y', height/2)
                     .text("Count");
 
     // Now, we will start actually building our scatterplot!
     var bar = svg.selectAll('.rect') // Select elements
                 .data(data);        // Bind data to elements
 
-    bar.enter().append('svg:rect'); // Create new elements if needed
+    // bar.enter().append('svg:rect'); // Create new elements if needed
 
     var tip = d3.tip()
         .attr('class', 'd3-tip')
@@ -101,14 +116,21 @@ function createProductsVis(){
     svg.call(tip);
 
     // Update our selection
-    bar
-        .attr('class', 'rect') // Give it a class
-        .attr('height', function(d) { return h-yScale(d.y)-yOffset-margin; })   // y-coordinate
-        // .attr('width', function(d){return 20})
-        .attr('x', function(d){ return xScale(d.idx) })
-        .attr('y', function(d){ return yScale(d.y)})
-        .append('svg:title') // tooltip
-        .text(function(d) { return "(x val="+d.product+")"});            
+    // bar
+    //     .attr('class', 'rect') // Give it a class
+    //     .attr('height', function(d) { return h-yScale(d.count)-yOffset-margin; })   // y-coordinate
+    //     // .attr('width', function(d){return 20})
+    //     .attr('x', function(d){ return xScale(d.idx) })
+    //     .attr('y', function(d){ return yScale(d.count)})
+    //     .append('svg:title') // tooltip
+    //     .text(function(d) { return "(x val="+d.product+")"});            
+    bar.enter().append("rect")
+      .attr("class", "rect")
+      .attr("x", function(d) { return xScale(d.product); })
+      .attr("y", function(d) { return yScale(d.count); })
+      .attr("height", function(d) { return height-yScale(d.count)-yOffset-margin; })
+      .attr("width", xScale.rangeBand());
+	    // .style("fill", function(d) { return color(d.name); });
 
     // Prettier tooltip
     bar.on('mouseover', function(d){
